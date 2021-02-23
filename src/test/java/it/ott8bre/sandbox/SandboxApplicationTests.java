eaa607b40f0658bc61958d4c7456c4d6b027f00b
Francesco Frosini <ott8bre@gmail.com>
2021-02-23 15:55:36 +0100

    initial import

---
+++ src/test/java/it/ott8bre/sandbox/SandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ott8bre.sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
